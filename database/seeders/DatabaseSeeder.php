304d7b66706e19e1227f7efbf0b0576b65e20c69
Ethan Brace <ethanabrace@gmail.com>
2022-03-13 10:11:51 -0400

    create  test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,17 +18,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->truncateTables();
+
         collect(['KFC', 'Taco Bell', 'Pizza Hut', 'The Habit Burger Grill'])
             ->each(fn($name) => Brand::factory()->name($name)->create());
 
-        \App\Models\User::factory(10)->create()->each(function(User $user) {
+        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'email'    => 'test@yum.yum',
+            'name'     => 'Mr Test',
+            'password' => bcrypt('test-password'),
+        ]);
+
+        User::all()->each(function(User $user) {
             $stores = Store::factory()
-                           ->has(Journal::factory()->count(3), 'journals')
+                           ->has(Journal::factory()->count(3), 'journal')
                            ->count(3)
                            ->create();
 
             $user->addStores($stores);
-
         });
+    }
+
+    private function truncateTables()
+    {
+        $tables = [
+            'store_user',
+            'journals',
+            'stores',
+            'brands',
+            'failed_jobs',
+            'password_resets',
+            'personal_access_tokens',
+            'users',
+        ];
+
+        foreach ($tables as $table) {
+            \DB::table($table)->delete();
+        }
     }
 }
