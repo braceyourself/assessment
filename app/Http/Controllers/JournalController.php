076e4e29edb81a192b90bd8fae90c0bd9a162383
Ethan Brace <ethanabrace@gmail.com>
2022-03-14 01:20:59 -0400

    add validation and optional filter

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Journal;
 use App\Models\Store;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class JournalController extends Controller
 {
@@ -15,38 +16,50 @@ class JournalController extends Controller
 
     public function index(Request $request, Store $store)
     {
+        $request->validate([
+            'interval' => [
+                Rule::in(Journal::intervals()->keys())
+            ]
+        ]);
+
         $this->authorize('view', $store);
 
-        return $store->journal()->paginate();
+        $query = $store->journal();
+
+        if ($request->has('interval')) {
+            $query->since($request->query('interval'));
+        }
+
+        return $query->paginate();
     }
 
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
+//    public function create()
+//    {
+//        //
+//    }
+//
+//    public function store(Request $request)
+//    {
+//        //
+//    }
 
     public function show(Journal $journal)
     {
         return $journal;
     }
 
-    public function edit(Journal $journal)
-    {
-        //
-    }
+//    public function edit(Journal $journal)
+//    {
 
-    public function update(Request $request, Journal $journal)
-    {
-        //
-    }
+//    }
 
-    public function destroy(Journal $journal)
-    {
-        //
-    }
+//    public function update(Request $request, Journal $journal)
+//    {
+
+//    }
+
+//    public function destroy(Journal $journal)
+//    {
+//        //
+//    }
 }
