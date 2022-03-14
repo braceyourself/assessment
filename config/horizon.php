f9672b50a9365b1a783fcfc6682864df8971e114
Ethan Brace <ethanabrace@gmail.com>
2022-03-14 01:00:07 -0400

    add mail queue + export route

--- config/horizon.php
+++ config/horizon.php
@@ -167,7 +167,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['default', 'mail'],
             'balance' => 'auto',
             'maxProcesses' => 1,
             'maxTime' => 0,
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('/brands', 'App\Http\Controllers\BrandController');
     Route::resource('/stores', 'App\Http\Controllers\StoreController');
     Route::resource('/stores/{store}/journal', 'App\Http\Controllers\JournalController');
-
+    Route::post('/stores/{store}/export', 'App\Http\Controllers\StoreDataExportController');
 });
